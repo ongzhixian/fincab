e02d8ed651f6db4f4fdc72742d5666f4775bba36
Zhixian Ong <zhixian.ong@ti8m.ch>
2022-12-02 12:04:45 +0800

    update

--- app/file_collectors/__init__.py
+++ app/file_collectors/__init__.py
@@ -6,11 +6,16 @@ from logger import Logger
 log = Logger()
 
 class AbstractFileDownloader(object):
-    def download_text(self, url):
+
+    def download(self, url):
+        
         request = self.make_url_request(url)
+        
         with url_open(request) as response:
             response_data = response.read()
+        
         return response_data
+
 
     def make_url_request(self, url):
         return url_request(
@@ -21,11 +26,17 @@ class AbstractFileDownloader(object):
             }
         )
 
-    def save_text_file(self, data, file_path_name):
+
+    def save_data_to_file(self, data, file_path_name):
+
         self.ensure_file_path_directory_exists(file_path_name)
+        
         with open(file_path_name, 'wb') as out_file:
             out_file.write(data)
 
+
     def ensure_file_path_directory_exists(self, file_path_name):
+
         file_dir_name = path.dirname(file_path_name)
+
         makedirs(file_dir_name, exist_ok=True)
--- app/file_collectors/mic_collector.py
+++ app/file_collectors/mic_collector.py
@@ -6,17 +6,45 @@ log = Logger()
 
 
 class MicFileCollector(AbstractFileDownloader):
+
     def __init__(self, output_path):
+
         self.output_path = output_path
+
         self.collect_mic_csv()
+        
+        self.collect_mic_excel()
 
     def collect_mic_csv(self):
+
         log.info("Collecting file", file_type="MIC CSV")
         
         save_file_path = path.join(self.output_path, "mic", "ISO10383_MIC.csv")
 
-        response_text_data = self.download_text('https://www.iso20022.org/sites/default/files/ISO10383_MIC/ISO10383_MIC.csv')
+        if path.exists(save_file_path):
+            log.info("Skip collecting file", file_type="MIC CSV")
+            return
 
-        self.save_text_file(response_text_data, save_file_path)
+        response_data = self.download('https://www.iso20022.org/sites/default/files/ISO10383_MIC/ISO10383_MIC.csv')
+
+        self.save_data_to_file(response_data, save_file_path)
         
         log.info("Collected file", file_type="MIC CSV")
+
+
+    def collect_mic_excel(self):
+
+        log.info("Collecting file", file_type="MIC Excel")
+        
+        save_file_path = path.join(self.output_path, "mic", "ISO10383_MIC.xlsx")
+
+        if path.exists(save_file_path):
+            log.info("Skip collecting file", file_type="MIC Excel")
+            return
+
+        response_data = self.download('https://www.iso20022.org/sites/default/files/ISO10383_MIC/ISO10383_MIC.xlsx')
+
+        self.save_data_to_file(response_data, save_file_path)
+        
+        log.info("Collected file", file_type="MIC Excel")
+    
